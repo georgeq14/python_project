2c2a3c2c951ba7f4fb7e408b13713ea34185a5e5
George Ian <geestillero@up.edu.ph>
2026-02-22 19:50:09 +0800

    file handling exer

---
+++ PYTHON_INTERMEDIATE/FileandErrorhandling/file_handling.py
@@ -0,0 +1,4 @@
+# This opens example file for writing
+file = open('example.txt', 'w')
+
+file.write('Hello, World! ')
---
+++ PYTHON_INTERMEDIATE/FileandErrorhandling/file_handling_exer.py
@@ -0,0 +1,11 @@
+'''
+file = open('Diary.txt', 'w')
+
+file.write("Hello diary this is me trying to learn how to be ablpe to code \n")
+file.write("Hello I am claiming that I would be able to graduate this year")
+'''
+
+# this si another way of how to be able to do it
+with open('diaries.txt', 'w') as file:
+    file.write('ashton is gay \n')
+    file.write('happy birthday alwyn')
